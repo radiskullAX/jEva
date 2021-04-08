7b4ba78af80ba24a1a874990c15a1301f29fb61b
radiskull <theradiskull2000@yahoo.de>
2021-04-08 02:02:37 +0200

    Refactorings
    - continued to refactor parsing/composing of irc messages
    - change any mode to enums

--- src/main/java/eu/animegame/jeva/core/IrcMessageComposer.java
+++ src/main/java/eu/animegame/jeva/core/IrcMessageComposer.java
@@ -4,6 +4,7 @@ import static java.util.stream.Collectors.joining;
 import java.util.Arrays;
 import java.util.stream.Stream;
 import eu.animegame.jeva.interfaces.IrcHandler;
+import eu.animegame.jeva.irc.UserMode;
 
 /**
  *
@@ -42,9 +43,36 @@ class IrcMessageComposer {
     handler.sendMessage(message.toString());
   }
 
-  public void sendUserMode(String nick, String mode) {
+  /**
+   * get a list of modes set for given user
+   * 
+   * @param nick
+   */
+  public void sendMode(String nick) {
     StringBuilder message = new StringBuilder();
-    message.append("MODE ").append(nick).append(" ").append(mode);
+    message.append("MODE ").append(nick);
+    handler.sendMessage(message.toString());
+  }
+
+  public void sendAddUserMode(String nick, String mode) {
+    sendMode(nick, "+", mode);
+  }
+
+  public void sendAddUserMode(String nick, UserMode mode) {
+    sendMode(nick, "+", mode.toString());
+  }
+
+  public void sendRemoveUserMode(String nick, String mode) {
+    sendMode(nick, "-", mode);
+  }
+
+  public void sendRemoveUserMode(String nick, UserMode mode) {
+    sendMode(nick, "-", mode.toString());
+  }
+
+  private void sendMode(String nick, String set, String mode) {
+    StringBuilder message = new StringBuilder();
+    message.append("MODE ").append(nick).append(" ").append(set).append(mode);
     handler.sendMessage(message.toString());
   }
 
@@ -57,7 +85,7 @@ class IrcMessageComposer {
   }
 
   public void sendQuit(String quitMessage) {
-    StringBuilder message = new StringBuilder("QUIT");
+    StringBuilder message = new StringBuilder();
     message.append("QUIT").append(" :").append(quitMessage);
     handler.sendMessage(message.toString());
   }
@@ -74,17 +102,16 @@ class IrcMessageComposer {
     handler.sendMessage(message.toString());
   }
 
+  public void sendJoin(String[] channels) {
+    sendJoin(Arrays.stream(channels).collect(joining(",")));
+  }
+
   public void sendJoin(String channel, String key) {
     StringBuilder message = new StringBuilder();
     message.append("JOIN ").append(channel).append(" ").append(key);
     handler.sendMessage(message.toString());
   }
 
-  public void sendJoin(String[] channels) {
-    StringBuilder message = new StringBuilder("JOIN ");
-    message.append(Arrays.stream(channels).collect(joining(",")));
-    handler.sendMessage(message.toString());
-  }
 
   /**
    * channels with passwords should come first
@@ -93,11 +120,11 @@ class IrcMessageComposer {
    * @param keys
    */
   public void sendJoin(String[] channels, String[] keys) {
-    StringBuilder message = new StringBuilder("JOIN ");
-    message.append(Arrays.stream(channels).collect(joining(",")));
-    message.append(" ");
-    message.append(Arrays.stream(keys).collect(joining(",")));
-    handler.sendMessage(message.toString());
+    sendJoin(Arrays.stream(channels).collect(joining(",")), Arrays.stream(keys).collect(joining(",")));
+  }
+
+  public void sendPartAll() {
+    handler.sendMessage("JOIN 0");
   }
 
   public void sendPart(String channel) {
@@ -107,9 +134,7 @@ class IrcMessageComposer {
   }
 
   public void sendPart(String[] channels) {
-    StringBuilder message = new StringBuilder();
-    message.append("PART ").append(Arrays.stream(channels).collect(joining(",")));
-    handler.sendMessage(message.toString());
+    sendPart(Arrays.stream(channels).collect(joining(",")));
   }
 
   public void sendPart(String channel, String partMessage) {
@@ -119,9 +144,7 @@ class IrcMessageComposer {
   }
 
   public void sendPart(String[] channels, String partMessage) {
-    StringBuilder message = new StringBuilder();
-    message.append("PART ").append(Arrays.stream(channels).collect(joining(","))).append(" :").append(partMessage);
-    handler.sendMessage(message.toString());
+    sendPart(Arrays.stream(channels).collect(joining(",")), partMessage);
   }
 
   public void sendChannelMode(String channel, String mode, String parameters) {
@@ -134,9 +157,9 @@ class IrcMessageComposer {
    * check the topic
    */
   public void sendTopic(String channel) {
-    StringBuilder output = new StringBuilder();
-    output.append("TOPIC ").append(channel);
-    handler.sendMessage(output.toString());
+    StringBuilder message = new StringBuilder();
+    message.append("TOPIC ").append(channel);
+    handler.sendMessage(message.toString());
   }
 
   /**
@@ -146,27 +169,140 @@ class IrcMessageComposer {
    * @param topic if empty, clear the topic on channel
    */
   public void sendTopic(String channel, String topic) {
-    StringBuilder output = new StringBuilder();
-    output.append("TOPIC ").append(channel).append(" :").append(topic);
-    handler.sendMessage(output.toString());
+    StringBuilder message = new StringBuilder();
+    message.append("TOPIC ").append(channel).append(" :").append(topic);
+    handler.sendMessage(message.toString());
+  }
+
+  /**
+   * list all channels + users
+   */
+  public void sendNames() {
+    handler.sendMessage("NAMES");
+  }
+
+  public void sendNames(String channel) {
+    sendNames(channel, "");
+  }
+
+  public void sendNames(String[] channels) {
+    sendNames(Arrays.stream(channels).collect(joining(",")), "");
+  }
+
+  public void sendNames(String channel, String target) {
+    StringBuilder message = new StringBuilder();
+    message.append("NAMES ").append(channel).append(" ").append(target);
+    handler.sendMessage(message.toString());
+  }
+
+  public void sendNames(String[] channels, String target) {
+    sendNames(Arrays.stream(channels).collect(joining(",")), target);
+  }
+
+  /**
+   * list all channels
+   */
+  public void sendList() {
+    handler.sendMessage("LIST");
+  }
+
+  public void sendList(String channel) {
+    sendList(channel, "");
+  }
+
+  public void sendList(String[] channels) {
+    sendList(Arrays.stream(channels).collect(joining(",")), "");
+  }
+
+  public void sendList(String channel, String target) {
+    StringBuilder message = new StringBuilder();
+    message.append("LIST ").append(channel).append(" ").append(target);
+    handler.sendMessage(message.toString());
+  }
+
+  public void sendList(String[] channels, String target) {
+    sendList(Arrays.stream(channels).collect(joining(",")), target);
+  }
+
+  public void sendInvite(String user, String channel) {
+    StringBuilder message = new StringBuilder();
+    message.append("INVITE ").append(user).append(" ").append(channel);
+    handler.sendMessage(message.toString());
+  }
+
+  public void sendKick(String channel, String user) {
+    StringBuilder message = new StringBuilder();
+    message.append("KICK ").append(channel).append(" ").append(user);
+    handler.sendMessage(message.toString());
+  }
+
+  public void sendKick(String[] channels, String[] users) {
+    sendKick(Arrays.stream(channels).collect(joining(",")), Arrays.stream(users).collect(joining(",")));
+  }
+
+  public void sendKick(String channel, String user, String kickMessage) {
+    StringBuilder message = new StringBuilder();
+    message.append("KICK ").append(channel).append(" ").append(user).append(" :").append(kickMessage);
+    handler.sendMessage(message.toString());
+  }
+
+  public void sendKick(String[] channels, String[] users, String message) {
+    sendKick(Arrays.stream(channels).collect(joining(",")), Arrays.stream(users).collect(joining(",")), message);
+  }
+
+  /**
+   * 
+   * @param target a channel or user
+   * @param privMessage
+   */
+  public void sendPrivMsg(String target, String privMessage) {
+    StringBuilder message = new StringBuilder();
+    message.append("PRIVMSG ").append(target).append(" :").append(privMessage);
+    handler.sendMessage(message.toString());
+  }
+
+  public void sendNotice(String target, String noticeMessage) {
+    StringBuilder message = new StringBuilder();
+    message.append("NOTICE ").append(target).append(" :").append(noticeMessage);
+    handler.sendMessage(message.toString());
+  }
+
+  public void sendMotd() {
+    sendMotd("");
+  }
+
+  public void sendMotd(String server) {
+    StringBuilder message = new StringBuilder();
+    message.append("MOTD ").append(server);
+    handler.sendMessage(message.toString());
+  }
+
+  public void sendLusers() {
+    handler.sendMessage("LUSERS");
+  }
+
+  public void sendLusers(String mask) {
+    sendLusers(mask, "");
+  }
+
+  public void sendLusers(String mask, String server) {
+    StringBuilder message = new StringBuilder();
+    message.append("LUSERS ").append(mask).append(" ").append(server);
+    handler.sendMessage(message.toString());
+  }
+
+  public void sendVersion() {
+    sendVersion("");
+  }
+
+  public void sendVersion(String server) {
+    StringBuilder message = new StringBuilder();
+    message.append("VERSION ").append(server);
+    handler.sendMessage(message.toString());
   }
 
 
 
-  public void sendPrivMsg(String channel, String message) {
-    StringBuilder output = new StringBuilder();
-    output.append("PRIVMSG ").append(channel).append(" :").append(message);
-    handler.sendMessage(output.toString());
-  }
-
-  public void sendKick(String channel, String user, String message) {
-    StringBuilder output = new StringBuilder();
-    output.append("KICK ").append(channel).append(" ").append(user);
-    if (message != null && !message.isEmpty()) {
-      output.append(" :").append(message);
-    }
-    handler.sendMessage(output.toString());
-  }
 
   public void sendPong(String pong) {
     StringBuilder output = new StringBuilder();
@@ -174,23 +310,9 @@ class IrcMessageComposer {
     handler.sendMessage(output.toString());
   }
 
-
-
-  public void sendInvite(String user, String channel) {
-    StringBuilder output = new StringBuilder();
-    output.append("INVITE ").append(user).append(" ").append(channel);
-    handler.sendMessage(output.toString());
-  }
-
   public void sendWhoIs(String user) {
     StringBuilder output = new StringBuilder();
     output.append("WHOIS ").append(user);
-    handler.sendMessage(output.toString());
-  }
-
-  public void sendNotice(String channel, String message) {
-    StringBuilder output = new StringBuilder();
-    output.append("NOTICE ").append(channel).append(" ").append(message);
     handler.sendMessage(output.toString());
   }
 
--- src/main/java/eu/animegame/jeva/irc/ChannelMode.java
+++ src/main/java/eu/animegame/jeva/irc/ChannelMode.java
@@ -1,9 +1,11 @@
 package eu.animegame.jeva.irc;
 
 /**
- *
+ * This enum defines all usable channel modes specified in RFC 2811.
+ * 
+ * @see <a href="https://tools.ietf.org/html/rfc2811#section-4">RFC 2811 s 4</a>
  * @author radiskull
  */
-public class ChannelMode {
+public enum ChannelMode {
 
 }
